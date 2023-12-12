9a577f2662d90a63ccddfae5b78eaa6f2661cc60
Fernando Nunes <fernandonunesjs@hotmail.com>
2023-12-12 12:38:08 -0300

    feat: start page end

--- quiz/app/page.tsx
+++ quiz/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div></div>
+    <div className="w-full">
+      <div className="w-full grid justify-center items-center h-screen">
+        <div>
+          <h1 className="text-center text-4xl font-bold">Quiz-Game</h1>
+          <p>Descubra quais livros vc deveria ler</p>
+          <div className="text-center p-2">
+            {/* <button className="bg-green-500 text-white p-2 rounded">Começar</button> */}
+            <Link href={"/quiz"} className="bg-green-500 text-white p-2 rounded">Começar</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
